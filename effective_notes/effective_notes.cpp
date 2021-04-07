a806352d637e6542989b239e344924e603b16ba6
Yyyyshen <Yyyyshen@gmail.com>
2021-04-07 15:33:39 +0800

    添加项目文件。

---
+++ effective_notes/effective_notes.cpp
@@ -0,0 +1,15 @@
+﻿// effective_notes.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * c++学习继续
+ * stl看的差不多，接着查缺补漏
+ * 本项目过一遍effective c++经典书系列
+ */
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
