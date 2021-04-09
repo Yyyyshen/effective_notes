d62c38d63a349187ddb0eaafd46ee2bcfb62d58c
Yyyyshen <Yyyyshen@gmail.com>
2021-04-09 14:58:29 +0800

    1.让自己习惯C++

---
+++ effective_cpp_02/effective_cpp_02.cpp
@@ -0,0 +1,14 @@
+﻿// effective_cpp_02.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 1.让自己习惯C++
+ * Accustoming Yourself to C++
+ */
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
