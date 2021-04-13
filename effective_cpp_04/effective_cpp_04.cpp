04ce69549a1a13f63e9b4cd6139ff8f806f35d2b
Yyyyshen <Yyyyshen@gmail.com>
2021-04-13 15:15:00 +0800

    3.资源管理

---
+++ effective_cpp_04/effective_cpp_04.cpp
@@ -0,0 +1,20 @@
+﻿// effective_cpp_04.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 3.资源管理
+ * Resource Management
+ */
+
+using namespace std;
+
+//
+//
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
