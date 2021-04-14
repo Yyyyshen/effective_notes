0f86900757b5a32077560b56d7967adb9a1b37dc
Yyyyshen <Yyyyshen@gmail.com>
2021-04-14 09:28:11 +0800

    4.设计与声明

---
+++ effective_cpp_05/effective_cpp_05.cpp
@@ -0,0 +1,21 @@
+﻿// effective_cpp_05.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 4.设计与声明
+ * Designs and Declarations
+ */
+
+using namespace std;
+
+//
+//
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
