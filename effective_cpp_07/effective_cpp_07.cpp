beb974f2724c459fe4c8541100ddf7eff8adf623
Yyyyshen <Yyyyshen@gmail.com>
2021-04-16 17:58:15 +0800

    6.继承与面向对象设计

---
+++ effective_cpp_07/effective_cpp_07.cpp
@@ -0,0 +1,24 @@
+﻿// effective_cpp_07.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 6.继承与面向对象设计
+ * Inheritance and Object-Oriented Dsign
+ */
+
+using namespace std;
+
+//
+//
+//
+
+
+
+
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
