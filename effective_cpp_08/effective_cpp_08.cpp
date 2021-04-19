786f27146e651c8e555a6790b520394e564ea863
Yyyyshen <Yyyyshen@gmail.com>
2021-04-19 15:23:19 +0800

    7.模板与泛型编程

---
+++ effective_cpp_08/effective_cpp_08.cpp
@@ -0,0 +1,24 @@
+﻿// effective_cpp_08.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 7.模板与泛型编程
+ * Templates and Generic Programming
+ */
+
+using namespace std;
+
+//
+//
+
+
+//
+//
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
