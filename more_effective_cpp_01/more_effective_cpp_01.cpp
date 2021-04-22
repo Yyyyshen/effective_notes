d61319a4ce32bd10c1db57d2235b82d57cea72eb
Yyyyshen <Yyyyshen@gmail.com>
2021-04-22 17:33:39 +0800

    参考《More Effective C++》

---
+++ more_effective_cpp_01/more_effective_cpp_01.cpp
@@ -0,0 +1,14 @@
+﻿// more_effective_cpp_01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 参考《More Effective C++》
+ */
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
