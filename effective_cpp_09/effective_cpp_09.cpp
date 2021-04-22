d790fa3e130da245076f228d8faa4a3013d9ecca
Yyyyshen <Yyyyshen@gmail.com>
2021-04-22 12:04:01 +0800

    8.定制new和delete

---
+++ effective_cpp_09/effective_cpp_09.cpp
@@ -0,0 +1,25 @@
+﻿// effective_cpp_09.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 8.定制new和delete
+ * Customizing new and delete
+ */
+
+using namespace std;
+
+//
+//
+
+
+//
+//
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
