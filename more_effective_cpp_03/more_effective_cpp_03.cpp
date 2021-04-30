eb20bb931e8b3faa04437074ba41539ccc60eb13
Yyyyshen <Yyyyshen@gmail.com>
2021-04-30 15:27:15 +0800

    异常

---
+++ more_effective_cpp_03/more_effective_cpp_03.cpp
@@ -0,0 +1,23 @@
+﻿// more_effective_cpp_03.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 异常
+ */
+
+using namespace std;
+
+//
+//
+
+
+//
+//
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
