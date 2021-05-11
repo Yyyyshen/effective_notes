8e0d2a3a4133b243d613b5a21fdaaf8e8f3ecbca
Yyyyshen <Yyyyshen@gmail.com>
2021-05-11 16:45:34 +0800

    杂项

---
+++ more_effective_cpp_06/more_effective_cpp_06.cpp
@@ -0,0 +1,22 @@
+﻿// more_effective_cpp_06.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 杂项
+ */
+
+using namespace std;
+
+//
+//
+
+
+//
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
