9255f79c82e817478b4e1f0f5c857af4b2aa3047
Yyyyshen <Yyyyshen@gmail.com>
2021-06-03 21:22:36 +0800

    6 函数子

---
+++ effective_stl_06/effective_stl_06.cpp
@@ -0,0 +1,23 @@
+﻿// effective_stl_06.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 6 函数子、函数子类、函数及其他
+ */
+
+using namespace std;
+
+//
+//
+
+
+//
+//
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
