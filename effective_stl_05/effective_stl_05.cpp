858b4f0d76055ccaaa73f547364e622e82dfab72
Yyyyshen <Yyyyshen@gmail.com>
2021-06-03 21:20:35 +0800

    第33~37条

--- effective_stl_05/effective_stl_05.cpp
+++ effective_stl_05/effective_stl_05.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <functional>
+#include <list>
+#include <set>
 
 /**
  * 5 算法
@@ -79,7 +82,144 @@ void test_erase_remove() {
 
 //第33条：对包含指针的容器使用remove这一类算法时要特别小心
 // 
+//看一个用vector存放指针的情况
+// 
+// 		+---+												   +---+
+// 		| 0 |  ------>  widget a							   | 0 |  ------>  widget a
+// 		|---|												   |---|
+// 		| 1 |  ------>  widget b （需要被删除） ---remove--->  | 1 |  ----	   widget b 
+// 		|---|												   |---|	  ↘
+// 		| 2 |  ------>  widget c							   | 2 |  ------>  widget c
+//		+---+												   +---+
+// 
+// 要被删除的指针（widget b）已经被不会被删除的指针覆盖了
+// 没有任何指针再指向它，这就发生了资源泄漏
+// 如果使用的是有引用计数功能的智能指针，可以直接使用erase-remove的习惯用法
 //
+
+
+//第34条：了解哪些算法要求使用排序的区间作为参数
+// 
+//罗列要求排序区间的STL算法
+// binary_search、lower_bound、upper_bound、equal_range、
+// set_union、set_intersection、set_difference、set_symmetric_difference
+// merge、inplace_merge、includes
+//另外，不一定要求排序区间，但通常情况下会与排序区间一起用的算法
+// unique、unique_copy
+// 
+//之所以要求排序区间，目的是为了提供更好的性能
+//只要确保提供排序区间，并保证这些算法使用的比较函数与排序使用的比较函数一致，就可以有效的执行操作
+//
+
+
+//第35条：通过mismatch或lexicographical_compare实现简单的忽略大小写的字符串比较
+// 
+//之前条例中提到的忽略大小写的方法
+int ci_char_compare(char c1, char c2) {
+	//tolower参数和返回值都是int，除非int是EOF，否则它的值必须可以用unsigned char表示，所以需要先强转
+	int ic1 = tolower(static_cast<unsigned char>(c1));
+	int ic2 = tolower(static_cast<unsigned char>(c2));
+	//都转换为小写后进行比较
+	if (ic1 < ic2) return -1;
+	if (ic1 > ic2) return 1;
+	return 0;
+}
+//可以根据mismatch算法，编写比较字符串的函数，但一般两字符串长度不一样，必须把短的字符串作为第一区间传入
+int ci_string_compare_impl(const string& s1, const string& s2);
+int ci_string_compare(const string& s1, const string& s2) {
+	if (s1.size() <= s2.size()) return ci_string_compare_impl(s1, s2);
+	else return -ci_string_compare_impl(s2, s1);
+}
+int ci_string_compare_impl(const string& s1, const string& s2) {
+	typedef pair<string::const_iterator, string::const_iterator> psci;
+	//mismatch返回一对迭代器，指示两个区间中对应字符第一次比较失败的位置
+	psci p = mismatch(s1.begin(), s1.end(), s2.begin(), not2(ptr_fun(ci_char_compare)));
+	if (p.first == s1.end())
+	{
+		//比较失败的位置在第一个区间结尾，要么是字符串相同，要么是s1更短
+		if (p.second == s2.end()) return 0;
+		else return -1;
+	}
+	return ci_char_compare(*p.first, *p.second);//如果不匹配出现在中间，则字符串之间关系和两个不匹配字符之间的关系相同
+}
+//第二种实现方式
+bool ci_char_less(char c1, char c2) {
+	return tolower(static_cast<unsigned char>(c1)) < tolower(static_cast<unsigned char>(c2));
+}
+bool ci_string_compare2(const string& s1, const string& s2) {
+	return lexicographical_compare(s1.begin(), s1.end(), s2.begin(), s2.end(), ci_char_less);
+}
+//还可以使用 strcmp
+int ci_string_compare3(const string& s1, const string& s2) {
+	return strcmp(s1.c_str(), s2.c_str());
+}
+//
+
+
+//第36条：理解copy_if算法的正确实现
+// 
+//在STL中，有11个名字中包含copy的算法
+// copy、copy_backward、replace_copy、reverse_copy、
+// replace_copy_if、unique_copy、remove_copy、rotate_copy
+// remove_copy_if、partial_sort_copy、uninitialized_copy
+//但却没有copy_if，所以，如果想复制区间中符合某条件的元素，则需要自己实现
+// 
+//一个看似合理的copy_if
+template<typename input_iterator, typename output_iterator, typename predicate>
+output_iterator copy_if(input_iterator begin, input_iterator end, output_iterator dest_begin, predicate p) {
+	return remove_copy_if(begin, end, dest_begin, not1(p));
+}
+//这种做法是以这样的事实为基础的：
+// 虽然STL不允许“复制所有使判别式条件为真的元素”
+// 但它允许“复制所有使判别式条件不为真的元素”
+// 所以，把判别式加上not1修饰后传递给remove_copy_if，就实现了copy_if
+// 然而，not1不能直接应用到一个函数指针上，必须先用ptr_fun进行转换
+// 为了调用copy_if这个实现，传入的不仅是一个函数对象，且应该是一个可配接的函数对象
+// 可以这样进行完善
+template<typename input_iterator, typename output_iterator, typename predicate>
+output_iterator copy_if(input_iterator begin, input_iterator end, output_iterator dest_begin, predicate p) {
+	while (begin != end) {
+		if (p(*begin)) *dest_begin++ = *begin;//手动遍历调用
+		++begin;
+	}
+	return dest_begin;
+}
+//
+
+
+//第37条：使用accumulate或者for_each进行区间统计
+// 
+//一些常见算法
+// count可以得出一个区间有多少元素，count_if则统计满足某个判别式的元素个数
+// min_element、max_element可以获取区间中的最大和最小值
+// 有时，需要自定义方式对区间进行统计处理
+// 例如计算一个容器中字符串长度综合、一个区间中数值乘积、平均值等
+// 可以使用accumulate
+#include <numeric> //不像其他算法那样存在于algorithm中
+void test_accumulate() {
+	list<double> list_d;
+	//...插入一些元素
+	double num = accumulate(list_d.begin(), list_d.end(), 0.0);
+	//这里0.0很重要，代表初始值为double，如果使用0，则最终计算结果会是int型，即使通过编译，结果也是错误的
+}
+//除了默认用法，还能自定义统计函数
+string::size_type string_length_sum(string::size_type sum_so_far, const string& s) {
+	return sum_so_far + s.size();
+}
+void test_accumulate2() {
+	set<string> set_str;
+	string::size_type len_sum = accumulate(set_str.begin(), set_str.end(), static_cast<string::size_type>(0), string_length_sum);
+}
+//计算乘积，直接用标准函数
+void test_accumulate3() {
+	vector<float> vec_f;
+	float ret = accumulate(vec_f.begin(), vec_f.end(), 1.0f, multiplies<float>());
+}
+//
+//for_each有相似的作用，但不同于accumulate直接返回统计结果，它返回一个函数对象，必须从中提取出所要的结果
+// 这也意味着，我们需要在函数子中加一个成员函数，来返回结果
+// 
+//（for_each函数参数允许有副作用，而accumulate不允许）
 //
 
 
