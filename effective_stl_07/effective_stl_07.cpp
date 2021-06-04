4915bfe91b53f13190fab95d5035a8adda13564e
Yyyyshen <Yyyyshen@gmail.com>
2021-06-04 15:10:27 +0800

    7 在程序中使用STL

---
+++ effective_stl_07/effective_stl_07.cpp
@@ -0,0 +1,23 @@
+﻿// effective_stl_07.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 7 在程序中使用STL
+ */
+
+using namespace std;
+
+//
+//
+
+
+//
+//
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
