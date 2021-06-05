233e068f502935470fe8ace99dafff748daddf2c
Yyyyshen <Yyyyshen@gmail.com>
2021-06-05 14:27:47 +0800

    参考《Exceptional C++》

---
+++ exceptional_cpp_01/exceptional_cpp_01.cpp
@@ -0,0 +1,17 @@
+﻿// exceptional_cpp_01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 参考《Exceptional C++》
+ */
+
+void* foo = nullptr;
+
+
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
