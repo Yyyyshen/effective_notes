fa8815ddc685afae812da5445a6f0e105c17258f
Yyyyshen <Yyyyshen@gmail.com>
2021-11-23 17:47:18 +0800

    《effective modern c++》学习

---
+++ effective_modern_cpp_01/effective_modern_cpp_01.cpp
@@ -0,0 +1,13 @@
+﻿// effective_modern_cpp_01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//
+//《effective modern c++》学习
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
